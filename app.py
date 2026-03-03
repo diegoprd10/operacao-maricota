a7af1f30735efa706e4b52fcf99deae847199af2
diego prado <diprads10@gmail.com>
2026-03-03 18:25:08 -0300

    primeiro deploy flask

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/missao")
+def missao():
+    return render_template("missao.html")
+
+@app.route("/missao1")
+def missao1():
+    return render_template("missao1.html")
+
+@app.route("/confirmacao", methods=["POST"])
+def confirmacao():
+    dia = request.form.get("dia")
+    horario = request.form.get("horario")
+
+    with open("respostas.txt", "a") as arquivo:
+        arquivo.write(f"Dia: {dia} | Horário: {horario}\n")
+
+    return redirect(url_for("aceita"))
+
+# ✅ Rota da página aceita
+@app.route("/aceita")
+def aceita():
+    return render_template("aceita.html")
+
+# ✅ Rota que recebe a decisão dos botões
+@app.route("/decisao", methods=["POST"])
+def decisao():
+    resposta = request.form.get("resposta")
+
+    with open("respostas.txt", "a") as arquivo:
+        arquivo.write(f"Resposta final: {resposta}\n")
+
+    return redirect(url_for("final"))
+
+# ✅ Última tela do projeto
+@app.route("/final")
+def final():
+    return render_template("final.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
